a5a824aca79b4179c41e79ddc161b2b1649417a9
harkame <louisr.daviaud@gmail.com>
2018-03-14 19:23:35 +0100

    add post report

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 import json
 import sqlite3
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -24,8 +24,9 @@ class Report(object):
         self.report_date = p_date
         self.report_confirm = p_confirm
 
-@app.route("/trams")
-def trams():
+#http://localhost:5000/get_trams
+@app.route("/get_trams")
+def get_trams():
     t_cursor.execute("SELECT * FROM tram")
 
     t_trams = list()
@@ -35,8 +36,9 @@ def trams():
 
     return json.dumps([ob.__dict__ for ob in t_trams])
 
-@app.route("/reports")
-def reports():
+#http://localhost:5000/get_reports
+@app.route("/get_reports")
+def get_reports():
     t_cursor.execute("SELECT * FROM report")
 
     t_reports = list()
@@ -45,3 +47,12 @@ def reports():
         t_reports.append(Report(t_report[0], t_report[1], t_report[2], t_report[3], t_report[4], t_report[5])) #Parsing of tuple to object
 
     return json.dumps([ob.__dict__ for ob in t_reports])
+
+#http://localhost:5000/post_report?report_id=42&report_stop=1&report_type=1&report_message="controleur"&report_date="26/04/2018"&report_confirm=1
+@app.route("/post_report")
+def post_report():
+    t_report = Report(int(request.args.get("report_id", None)), int(request.args.get("report_stop", None)), int(request.args.get("report_type", None)), request.args.get("report_message", None), request.args.get("report_date", None), int(request.args.get("report_confirm", None)))
+
+    t_cursor.execute("INSERT INTO report (report_id, report_stop, report_type, report_message, report_date, report_confirm) VALUES (?, ?, ?, ?, ?, ?)", (t_report.report_id, t_report.report_stop,  t_report.report_type, t_report.report_message, t_report.report_date, t_report.report_confirm))
+    t_connection.commit()
+    return "ok";
