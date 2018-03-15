50a80a4520902215112463b5f5c3a8cb3db36920
flyingrub <flyingrub@protonmail.com>
2018-03-15 17:06:39 +0100

    Some cleanup

--- src/server.py
+++ src/server.py
@@ -12,20 +12,20 @@ print("[Database connection OK]");
 #TODO : Move this class in tram.py
 class Tram(object):
     def __init__(self, p_id, p_occupation):
-        self.tram_id = p_id
-        self.tram_occupation = p_occupation
+        self.id = p_id
+        self.occupation = p_occupation
 
 class Report(object):
     def __init__(self, p_id, p_stop, p_type, p_message, p_date, p_confirm):
-        self.report_id = p_id
-        self.report_stop = p_stop
-        self.report_type = p_type
-        self.report_message = p_message
-        self.report_date = p_date
-        self.report_confirm = p_confirm
+        self.id = p_id
+        self.stop = p_stop
+        self.type = p_type
+        self.message = p_message
+        self.date = p_date
+        self.confirm = p_confirm
 
 #http://localhost:5000/get_trams
-@app.route("/get_trams")
+@app.route("/trams", methods=['GET'])
 def get_trams():
     t_cursor.execute("SELECT * FROM tram")
 
@@ -36,8 +36,8 @@ def get_trams():
 
     return json.dumps([ob.__dict__ for ob in t_trams])
 
-#http://localhost:5000/get_reports
-@app.route("/get_reports")
+#http://localhost:5000/reports
+@app.route("/reports", methods=['GET'])
 def get_reports():
     t_cursor.execute("SELECT * FROM report")
 
@@ -48,11 +48,11 @@ def get_reports():
 
     return json.dumps([ob.__dict__ for ob in t_reports])
 
-#http://localhost:5000/post_report?report_id=42&report_stop=1&report_type=1&report_message="controleur"&report_date="26/04/2018"&report_confirm=1
-@app.route("/post_report")
+#http://localhost:5000/report?report_id=42&report_stop=1&report_type=1&report_message="controleur"&report_date="26/04/2018"&report_confirm=1
+@app.route("/report", methods=['POST'])
 def post_report():
     t_report = Report(int(request.args.get("report_id", None)), int(request.args.get("report_stop", None)), int(request.args.get("report_type", None)), request.args.get("report_message", None), request.args.get("report_date", None), int(request.args.get("report_confirm", None)))
 
-    t_cursor.execute("INSERT INTO report (report_id, report_stop, report_type, report_message, report_date, report_confirm) VALUES (?, ?, ?, ?, ?, ?)", (t_report.report_id, t_report.report_stop,  t_report.report_type, t_report.report_message, t_report.report_date, t_report.report_confirm))
+    t_cursor.execute("INSERT INTO report (report_id, report_stop, report_type, report_message, report_date, report_confirm) VALUES (?, ?, ?, ?, ?, ?)", (t_report.id, t_report.stop,  t_report.type, t_report.message, t_report.date, t_report.confirm))
     t_connection.commit()
     return "ok";
